0156d693bb7dbec53b1361c159f7100174628ac9
JackCurrie <jack.currie@nextgi.com>
2017-02-10 16:49:05 -0800

    template for add view created and implemented

--- treehouse-flask.py
+++ treehouse-flask.py
@@ -1,6 +1,5 @@
-
 from flask import Flask
-
+from flask import render_template
 
 # Refer to own namespace
 app = Flask(__name__)
@@ -18,7 +17,8 @@ def index(name="Currie"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return "{} + {} = {}".format(num1, num2, num1+num2)
+    context = {"num1": num1, "num2": num2}
+    return render_template("add.html", **context)
 
 @app.route('/multiply/<int:num1>/<int:num2>')
 @app.route('/multiply/<float:num1>/<float:num2>')
